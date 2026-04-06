015b1eb8eb10b5ff160a76e55f432727e40bbe29
Aman Shahi <amanshahidev@gmail.com>
2026-04-06 09:16:44 +0530

    added insightsSlice with memoized selectors for all derived analytics

---
+++ src/store/insightsSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const insightsSlice = createSlice({
+  name: "insights",
+  initialState: {},
+  reducers: {},
+});
+
+export default insightsSlice.reducer;
